3534e825bdc03e0ffcc3c1d2a7fc6752e10d0ce2
Pumpkinouu <zivanovicdanilo0@gmail.com>
2023-01-31 23:06:40 +0100

    Added the whole project

---
+++ IMC_DaniloZivanovic/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace IMC_DaniloZivanovic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ETML
+            //Auteur : Danilo Zivanovic
+            //Date   : 11.11.2022
+           
+            Console.Title = "Calculateur IMC";
+
+            Console.WriteLine("╔════════════════════════════════════════════╗");
+            Console.WriteLine("║              Calculateur IMC               ║");
+            Console.WriteLine("╚════════════════════════════════════════════╝");
+
+           
+            //Commande pour faire les variables
+            int poids;
+            float taille;
+
+
+            
+            Console.WriteLine("Entrez votre taille (Exemple : 1,74) :");
+            Console.SetCursorPosition(38, 3);//Permet de déplacer le curseur
+
+
+
+            string resultatTaille = Console.ReadLine();
+           
+            taille = Convert.ToSingle(resultatTaille);
+            
+
+            Console.Write("Entrez votre poids :");
+            string resultatPoids = Console.ReadLine();
+            poids = Convert.ToInt32(resultatPoids);
+            float imc = poids / (taille * taille);
+            Console.WriteLine("Voici votre imc : {0}",imc);
+            Console.ReadLine();
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
